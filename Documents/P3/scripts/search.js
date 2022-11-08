48020e08aa24d36ece08022d9720ee37095815ad
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2022-11-08 21:18:40 +0100

    added search to index.html and fixed styling

--- Documents/P3/scripts/search.js
+++ Documents/P3/scripts/search.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function search() {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById('searchInput');
@@ -7,13 +7,20 @@ function myFunction() {
   li = ul.getElementsByTagName('li');
 
   // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
+  if(filter==""){
+    for (i = 0; i < li.length; i++) {
       li[i].style.display = "none";
+    }
+  }
+  else {
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
     }
   }
 }
