9e9692cb0629822ca2e717ae0d20b9de86087e7c
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2022-11-19 19:00:02 +0100

    pullItems works, and header-push

--- Documents/P3/scripts/inSeason.js
+++ Documents/P3/scripts/inSeason.js
@@ -1,21 +1,21 @@
 
 
-const itemList = document.getElementsByClassName("item-list");
+// const itemList = document.getElementsByClassName("item-list");
 
-function listHandler() {
+// function listHandler() {
     
-    for(i=0;i<items.length;i++) {
-        const listItem = document.createElement("li");
-        const a = document.createElement("a");
-        const img = document.createElement("img");
-        listItem.className = "item";
-        a.href = items[i].href + ".html";
-        img.src = "Images/" + items[i].name + "/" + items[i].name + ".jpg";
-        a.appendChild(img);
-        listItem.appendChild(a);
+//     for(i=0;i<items.length;i++) {
+//         const listItem = document.createElement("li");
+//         const a = document.createElement("a");
+//         const img = document.createElement("img");
+//         listItem.className = "item";
+//         a.href = items[i].href + ".html";
+//         img.src = "Images/" + items[i].name + "/" + items[i].name + ".jpg";
+//         a.appendChild(img);
+//         listItem.appendChild(a);
 
-        console.log(listItem);
-    }
+//         console.log(listItem);
+//     }
     
-};
-console.log("out: ", items);
+// };
+// console.log("out: ", items);
