5556ca68fe97008e581a14d69b2962982bbcd818
Caroline <carolisb@stud.ntnu.no>
2022-11-20 15:51:55 +0100

    p4 final thoughts

--- Documents/P3/scripts/inSeason.js
+++ Documents/P3/scripts/inSeason.js
@@ -4,32 +4,6 @@
 
 // // function listHandler() {
     
-<<<<<<< HEAD
-//     items.forEach(item => {
-//         const listItem = document.createElement("li");
-//         const a = document.createElement("a");
-//         const img = document.createElement("img");
-//         const article = document.createElement("article");
-//         const artHeader = document.createElement("h3");
-//         const artParagraph = document.createElement("p")
-//         listItem.className = "item";
-//         a.href = item.href + ".html";
-//         Object.assign(img, {
-//             src: "images/" + item.type + "/" + item.href + ".png",
-//             alt: item.name,
-//             height: 100,
-//             width: 100
-//         })
-//         artHeader.innerHTML = item.name;
-//         artParagraph.innerHTML = item.description;
-//         article.appendChild(artHeader);
-//         article.appendChild(artParagraph)
-//         a.appendChild(img);
-//         listItem.appendChild(a);
-//         listItem.appendChild(article);
-//         document.getElementById("berry-list").appendChild(listItem);
-//     });
-=======
     for(i=0;i<items.length;i++) {
         const listItem = document.createElement("li");
         const a = document.createElement("a");
@@ -54,7 +28,6 @@
         listItem.appendChild(article);
         document.getElementById("berryList").appendChild(listItem);
     }
->>>>>>> 96b6ea35391946e6ccca028758d4832b94525794
     
 
 // console.log(infoItems[0].language[1]);
